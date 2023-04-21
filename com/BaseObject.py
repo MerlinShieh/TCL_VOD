47e908cbc3f1e24d47062b69c1df1bc49fc1e3fb
MerlinRedmi <merlinshieh@outlook.com>
2023-04-21 19:17:54 +0800

    没啥用

---
+++ com/BaseObject.py
@@ -0,0 +1,18 @@
+from tools.keyMap import Key
+
+
+def paly():
+    Key.right()
+    Key.down()
+    Key.down()
+    Key.down()
+    Key.down()
+    Key.down()
+    Key.enter()
+    return True
+
+
+def cashDesk():
+    Key.right()
+    Key.up()
+    return True
